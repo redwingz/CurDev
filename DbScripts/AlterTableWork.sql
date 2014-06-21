ae097fa16c99fac2c98d13012d75dac004584248
redwingz <tim.g.percival@gmail.com>
2014-06-21 13:02:45 +1200

    Scripting work
    Scripting work (cont.)

---
+++ DbScripts/AlterTableWork.sql
@@ -0,0 +1,12 @@
+﻿/*
+alter table Work
+drop constraint fk_Work_Artist 
+
+alter table Work
+add constraint fk_Work_Artist foreign key (artistname) 
+references artist (name) on delete cascade
+*/
+select * from work;
+select * from work;
+
+
